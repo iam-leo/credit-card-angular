f15a247dff75652e974ffbd13eb41e1d18f26727
Javier Leonel Sosa <leo.vm.cba@gmail.com>
2023-10-31 17:42:55 -0300

    add: form validations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { CrearTarjetaComponent } from './components/crear-tarjeta/crear-tarjeta.component';
@@ -12,7 +13,8 @@ import { ListarTarjetasComponent } from './components/listar-tarjetas/listar-tar
     ListarTarjetasComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
